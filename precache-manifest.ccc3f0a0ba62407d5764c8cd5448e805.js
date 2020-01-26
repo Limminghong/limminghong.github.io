42f2b9456bef27be516332e867ac01646bcafe38
Limminghong <minghonglim@gmail.com>
2020-01-26 20:00:42 +0800

    Updates

---
+++ precache-manifest.ccc3f0a0ba62407d5764c8cd5448e805.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3a59ded196673414a74b79383e1e17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4b42755c7ee974dc30",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "cdc76cf35d35e49f33b3",
+    "url": "/static/css/main.b3406459.chunk.css"
+  },
+  {
+    "revision": "4d4b42755c7ee974dc30",
+    "url": "/static/js/2.c1adcd8f.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.c1adcd8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdc76cf35d35e49f33b3",
+    "url": "/static/js/main.a1918602.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
